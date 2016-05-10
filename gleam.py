c0ebb8421484fb026d3292d750bdb1c551eb1397
drumbot <mailhot.hugo@gmail.com>
2016-05-09 17:50:55 -0700

    created GleamModel class

--- gleam.py
+++ gleam.py
@@ -13,103 +13,109 @@ Journal of Computational Science 1 (3): 132–45.
 
 from copy import deepcopy
 import json
-import pandas as pd
 import math
 
 
-def compute_infection_rate(comps, v):
-    return v['transmission_rate'] * infectious_count(comps) / v['total_pop']
+class GleamModel(object):
+
+    def __init__(self):
+        pass
+
+    def compute_infection_rate(self):
+        return (self.params['transmission_rate'] * self.infectious_count() /
+                sum(self.comps.values()))
+
+    def get_state(self):
+        """
+        Returns a copy of the model compartments.
+        """
+        return deepcopy(self.comps)
+
+    def infectious_count(self):
+        return (self.comps['symptomatic_travel'] +
+                self.comps['symptomatic_no_travel'] +
+                self.comps['asymptomatic'])
+
+    def infect(self):
+        """
+        Updates counts in compartements according to model parameters.
+        """
+
+        inf_rate = self.compute_infection_rate(self.comps, self.params)
+        new_latent = math.ceil(self.comps['susceptible'] * inf_rate)
+        new_sym_no_travel = math.ceil(self.comps['latent'] *
+                                      self.params['p_exit_latent'] *
+                                      (1 - self.params['p_asymptomatic_infection']) *
+                                      (1 - self.params['p_travel_permission']))
+        new_sym_travel = math.ceil(self.comps['latent'] *
+                                   self.params['p_exit_latent'] *
+                                   (1 - self.params['p_asymptomatic_infection']) *
+                                   self.params['p_travel_permission'])
+        new_asymptomatic = math.ceil(self.comps['latent'] *
+                                     self.params['p_exit_latent'] *
+                                     self.params['p_asymptomatic_infection'])
+        new_snt_recovered = math.ceil(self.comps['symptomatic_no_travel'] *
+                                      self.params['p_recovery'])
+        new_st_recovered = math.ceil(self.comps['symptomatic_travel'] *
+                                     self.params['p_recovery'])
+        new_asym_recovered = math.ceil(self.comps['asymptomatic'] *
+                                       self.params['p_recovery'])
+
+        self.comps['susceptible'] -= new_latent
+        self.comps['latent'] += new_latent
+        self.comps['latent'] -= (new_sym_no_travel + new_sym_travel +
+                                 new_asymptomatic)
+        self.comps['symptomatic_no_travel'] += new_sym_no_travel
+        self.comps['symptomatic_travel'] += new_sym_travel
+        self.comps['asymptomatic'] += new_asymptomatic
+        self.comps['symptomatic_no_travel'] -= new_snt_recovered
+        self.comps['symptomatic_travel'] -= new_st_recovered
+        self.comps['asymptomatic'] -= new_asym_recovered
+        self.comps['recovered'] += (new_snt_recovered + new_st_recovered +
+                                    new_asym_recovered)
+
+    def set_compartments(self, comps):
+        for x in ['susceptible', 'latent', 'symptomatic_no_travel',
+                  'symptomatic_travel', 'asymptomatic', 'recovered']:
+            if x not in comps.keys():
+                raise Exception("GleamModel copartments must include {}".format(x))
+        self.comps = comps
+
+    def set_model_params(self, params):
+        for x in ['p_exit_latent', 'transmission_rate', 'p_asymptomatic_infection',
+                  'p_travel_permission', 'p_recovery']:
+            if x not in params.keys():
+                raise Exception("GleamModel parameters must include {}".format(x))
+        self.params = params
 
 
-def infect(comps, v):
-    """
-    Updates counts in compartements (comps) according to model variables (v).
-    """
-
-    inf_rate = compute_infection_rate(comps, v)
-    new_latent = math.ceil(comps['susceptible'] * inf_rate)
-    new_symptomatic_no_travel = math.ceil(comps['latent'] *
-                                    v['p_exit_latent'] *
-                                    (1 - v['p_asymptomatic_infection']) *
-                                    (1 - v['p_travel_permission']))
-    new_symptomatic_travel = math.ceil(comps['latent'] *
-                                 v['p_exit_latent'] *
-                                 (1 - v['p_asymptomatic_infection']) *
-                                 v['p_travel_permission'])
-    new_asymptomatic = math.ceil(comps['latent'] *
-                           v['p_exit_latent'] *
-                           v['p_asymptomatic_infection'])
-    new_snt_recovered = math.ceil(comps['symptomatic_no_travel'] * v['p_recovery'])
-    new_st_recovered = math.ceil(comps['symptomatic_travel'] * v['p_recovery'])
-    new_asym_recovered = math.ceil(comps['asymptomatic'] * v['p_recovery'])
-
-    comps['susceptible'] -= new_latent
-    comps['latent'] += new_latent
-    comps['latent'] -= (new_symptomatic_no_travel + new_symptomatic_travel +
-                        new_asymptomatic)
-    comps['symptomatic_no_travel'] += new_symptomatic_no_travel
-    comps['symptomatic_travel'] += new_symptomatic_travel
-    comps['asymptomatic'] += new_asymptomatic
-    comps['symptomatic_no_travel'] -= new_snt_recovered
-    comps['symptomatic_travel'] -= new_st_recovered
-    comps['asymptomatic'] -= new_asym_recovered
-    comps['recovered'] += new_snt_recovered + new_st_recovered + new_asym_recovered
 
 
-def infectious_count(comps):
-    return (comps['symptomatic_travel'] +
-            comps['symptomatic_no_travel'] +
-            comps['asymptomatic'])
 
-
-def print_graph(comps):
-    """
-    Prints a horizontal bar chart of the current compartment counts.
-    """
-    print("""
-          susceptible:   {}  {}
-          latent:        {}  {}
-          sym_no_travel: {}  {}
-          sym_travel:    {}  {}
-          asymptomatic:  {}  {}
-          recovered:     {}  {}
-          """.format('-' * int((comps['susceptible'] // 13)),
-                     int(comps['susceptible']),
-                     '-' * int((comps['latent'] // 13)),
-                     int(comps['latent']),
-                     '-' * int((comps['symptomatic_no_travel'] // 13)),
-                     int(comps['symptomatic_no_travel']),
-                     '-' * int((comps['symptomatic_travel'] // 13)),
-                     int(comps['symptomatic_travel']),
-                     '-' * int((comps['asymptomatic'] // 13)),
-                     int(comps['asymptomatic']),
-                     '-' * int((comps['recovered'] // 13)),
-                     int(comps['recovered']))
-          )
-
-model_variables = {'p_exit_latent': 0.2,
-                   'transmission_rate': 0.6,
-                   'total_pop': 2000,
-                   'p_asymptomatic_infection': 0.1,
-                   'p_travel_permission': 0.5,
-                   'p_recovery': 0.1}
-
-compartments = {'susceptible': 1950,
-                'latent': 0,
-                'symptomatic_no_travel': 10,
-                'symptomatic_travel': 0,
-                'asymptomatic': 0,
-                'recovered': 0}
 
 
 if __name__ == '__main__':
-    print_graph(compartments)
+    parameters = {'p_exit_latent': 0.2,
+                  'transmission_rate': 0.6,
+                  'total_pop': 2000,
+                  'p_asymptomatic_infection': 0.1,
+                  'p_travel_permission': 0.5,
+                  'p_recovery': 0.1}
+
+    compartments = {'susceptible': 1950,
+                    'latent': 0,
+                    'symptomatic_no_travel': 10,
+                    'symptomatic_travel': 0,
+                    'asymptomatic': 0,
+                    'recovered': 0}
+    gm = GleamModel()
+    gm.set_compartments(compartments)
+    gm.set_parameters(parameters)
     results = []
     for i in range(1000):
-        results.append(deepcopy(compartments))
+        results.append(gm.get_state())
         # print('total pop: {}'.format(sum(compartments.values())))
-        infect(compartments, model_variables)
-    print_graph(compartments)
+        gm.infect()
     with open('results.json', 'w') as f:
         json.dump(results, f)
 
